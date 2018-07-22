31aa8c3deeabc02c4336835f6e02f21271072d97
patricksutunya <psutunya@aol.com>
2018-07-22 14:49:29 -0400

    Initial Commit

---
+++ TodoTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoTask
+//
+//  Created by Patrick Sutunya on 7/22/18.
+//  Copyright © 2018 psutunya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

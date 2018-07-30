4a7aa2f47b518c12b66221540dd800ff54ad0ffa
patricksutunya <psutunya@aol.com>
2018-07-30 18:41:57 -0400

    Complete SwipeCellKit on CategoryViewController

--- TodoTask/Controllers/CategoryTableViewController.swift
+++ TodoTask/Controllers/CategoryTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import RealmSwift
-
+import SwipeCellKit
 
 
 class CategoryTableViewController: UITableViewController {
@@ -21,8 +21,9 @@ class CategoryTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
         loadCategories()
+        tableView.rowHeight = 80.0
+        
     }
     // MARK: - TableView Datasource Methods
     
@@ -32,10 +33,11 @@ class CategoryTableViewController: UITableViewController {
    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath) as! SwipeTableViewCell
        
         cell.textLabel?.text = categoryArray?[indexPath.row].name ?? "No Categories Added Yet"
+        cell.delegate = self
+        
         return cell
     }
     
@@ -104,9 +106,46 @@ class CategoryTableViewController: UITableViewController {
         
         categoryArray = realm.objects(Category.self)
         tableView.reloadData()
-      
-        
+   
     }
- 
     
 }
+
+// MARK: - Swipe Cell Delegate Methods
+
+extension CategoryTableViewController: SwipeTableViewCellDelegate {
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            
+            if let categoryForDeletion = self.categoryArray?[indexPath.row]{
+                do {
+                    try self.realm.write {
+                     self.realm.delete(categoryForDeletion)
+                    }
+                } catch {
+                    print("Error deleting \(error)")
+                }
+                
+                tableView.reloadData()
+            }
+            
+    }
+        
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        
+        return [deleteAction]
+    }
+    
+  /* func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        //options.transitionStyle = .border
+        return options
+    } */
+}
+
--- TodoTask/Controllers/TodoListViewController.swift
+++ TodoTask/Controllers/TodoListViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import RealmSwift
 
 
+
 class TodoListViewController: UITableViewController {
 
     var todoItems : Results<Item>?
